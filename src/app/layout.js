231f45d2e0f0fc4413fe5a01cc146fde38bc9d7b
zjkl0330 <827802685@qq.com>
2025-11-21 18:31:04 +0800

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{ backgroundImage: 'url(https://t.alcy.cc/ys)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }} className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="G-JVKEXR5XSG" />
     </html>
   );
